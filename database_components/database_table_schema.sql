b55c2e32808c85d6fef54b687f15757a74f4a52f
adebayodeji <adebyodeji14@yahoo.com>
2023-12-02 19:57:27 +0200

    Updated script file to be run once

--- database_components/database_table_schema.sql
+++ database_components/database_table_schema.sql
@@ -2,19 +2,7 @@
 
 -- DROP DATABASE IF EXISTS db_rental;
 
-CREATE DATABASE db_rental
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-
-COMMENT ON DATABASE db_rental
-    IS 'rental app development project ';
-
+CREATE DATABASE db_rental;
 
 ----Script for Sequence-------------
 -- SEQUENCE: public.tbl_house_mate_housemate_id_seq
@@ -26,11 +14,9 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_house_mate_housemate_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_house_mate.housemate_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_house_mate_housemate_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_house_mate_housemate_id_seq OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_house_mate_user_id_seq
 
@@ -41,11 +27,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_house_mate_user_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_house_mate.user_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_house_mate_user_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_house_mate_user_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_maintenance_issue_id_seq
 
@@ -56,11 +41,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_maintenance_issue_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_maintenance.issue_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_maintenance_issue_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_maintenance_issue_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_maintenance_property_id_seq
 
@@ -71,11 +55,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_maintenance_property_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_maintenance.property_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_maintenance_property_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_maintenance_property_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_property_property_id_seq
 
@@ -86,11 +69,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_property_property_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_property.property_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_property_property_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_property_property_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_property_user_id_seq
 
@@ -101,11 +83,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_property_user_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_property.user_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_property_user_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_property_user_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_rental_agreement_agreement_id_seq
 
@@ -116,11 +97,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_rental_agreement_agreement_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_rental_agreement.agreement_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_rental_agreement_agreement_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_rental_agreement_agreement_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_rental_agreement_property_id_seq
 
@@ -131,11 +111,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_rental_agreement_property_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_rental_agreement.property_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_rental_agreement_property_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_rental_agreement_property_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_rental_agreement_user_id_seq
 
@@ -146,11 +125,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_rental_agreement_user_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_rental_agreement.user_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_rental_agreement_user_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_rental_agreement_user_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_support_support_id_seq
 
@@ -161,11 +139,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_support_support_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_support.support_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_support_support_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_support_support_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_support_user_id_seq
 
@@ -176,11 +153,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_support_user_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_support.user_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_support_user_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_support_user_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_user_user_id_seq
 
@@ -191,11 +167,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_user_user_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_user.user_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_user_user_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_user_user_id_seq
+    --OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_verification_user_id_seq
 
@@ -206,11 +181,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_verification_user_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_verification.user_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_verification_user_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_verification_user_id_seq
+   -- OWNER TO postgres;
 
 -- SEQUENCE: public.tbl_verification_verification_id_seq
 
@@ -221,11 +195,10 @@ CREATE SEQUENCE IF NOT EXISTS public.tbl_verification_verification_id_seq
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
-    CACHE 1
-    OWNED BY tbl_verification.verification_id;
+    CACHE 1;
 
-ALTER SEQUENCE public.tbl_verification_verification_id_seq
-    OWNER TO postgres;
+--ALTER SEQUENCE public.tbl_verification_verification_id_seq
+   -- OWNER TO postgres;
 
 	
 	
@@ -238,23 +211,22 @@ ALTER SEQUENCE public.tbl_verification_verification_id_seq
 CREATE TABLE IF NOT EXISTS public.tbl_user
 (
     user_id integer NOT NULL DEFAULT nextval('tbl_user_user_id_seq'::regclass),
-    name character varying(255) COLLATE pg_catalog."default",
-    email character varying(255) COLLATE pg_catalog."default",
-    contact character varying(255) COLLATE pg_catalog."default",
-    academic_institution character varying(255) COLLATE pg_catalog."default",
-    address character varying(255) COLLATE pg_catalog."default",
+    name character varying(255),
+    email character varying(255),
+    contact character varying(255),
+    academic_institution character varying(255),
+    address character varying(255),
     picture bytea,
-    user_type character varying(255) COLLATE pg_catalog."default",
-    status character varying(255) COLLATE pg_catalog."default",
-    rating character varying(255) COLLATE pg_catalog."default",
+    user_type character varying(255),
+    status character varying(255),
+    rating character varying(255),
     "timestamp" time without time zone,
     CONSTRAINT tbl_user_pkey PRIMARY KEY (user_id)
 )
 
-TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.tbl_user
-    OWNER to postgres;
+--ALTER TABLE IF EXISTS public.tbl_user
+   -- OWNER to postgres;
 	
 ------Script for tbl_property-----------
 
@@ -265,16 +237,16 @@ ALTER TABLE IF EXISTS public.tbl_user
 CREATE TABLE IF NOT EXISTS public.tbl_property
 (
     property_id integer NOT NULL DEFAULT nextval('tbl_property_property_id_seq'::regclass),
-    property_name character varying(255) COLLATE pg_catalog."default",
-    property_address character varying(255) COLLATE pg_catalog."default",
-    property_type character varying(255) COLLATE pg_catalog."default",
-    rental_price character varying(255) COLLATE pg_catalog."default",
-    property_details character varying(255) COLLATE pg_catalog."default",
-    rental_terms character varying(255) COLLATE pg_catalog."default",
+    property_name character varying(255),
+    property_address character varying(255),
+    property_type character varying(255),
+    rental_price character varying(255),
+    property_details character varying(255),
+    rental_terms character varying(255),
     property_image bytea,
-    additional_bills character varying(255) COLLATE pg_catalog."default",
-    not_allowed character varying(255) COLLATE pg_catalog."default",
-    availability character varying(255) COLLATE pg_catalog."default",
+    additional_bills character varying(255),
+    not_allowed character varying(255),
+    availability character varying(255),
     user_id integer NOT NULL DEFAULT nextval('tbl_property_user_id_seq'::regclass),
     "timestamp" time without time zone,
     CONSTRAINT tbl_property_pkey PRIMARY KEY (property_id),
@@ -284,10 +256,10 @@ CREATE TABLE IF NOT EXISTS public.tbl_property
         ON DELETE NO ACTION
 )
 
-TABLESPACE pg_default;
+--TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.tbl_property
-    OWNER to postgres;
+--ALTER TABLE IF EXISTS public.tbl_property
+    --OWNER to postgres;
 	
 -------Script House_Mate-----------------
 
@@ -299,8 +271,8 @@ CREATE TABLE IF NOT EXISTS public.tbl_house_mate
 (
     housemate_id integer NOT NULL DEFAULT nextval('tbl_house_mate_housemate_id_seq'::regclass),
     user_id integer NOT NULL DEFAULT nextval('tbl_house_mate_user_id_seq'::regclass),
-    preferences character varying(255) COLLATE pg_catalog."default",
-    agreement_details character varying(255) COLLATE pg_catalog."default",
+    preferences character varying(255),
+    agreement_details character varying(255),
     CONSTRAINT tbl_house_mate_pkey PRIMARY KEY (housemate_id),
     CONSTRAINT user_id_foreign_key FOREIGN KEY (user_id)
         REFERENCES public.tbl_user (user_id) MATCH SIMPLE
@@ -308,10 +280,10 @@ CREATE TABLE IF NOT EXISTS public.tbl_house_mate
         ON DELETE NO ACTION
 )
 
-TABLESPACE pg_default;
+--TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.tbl_house_mate
-    OWNER to postgres;
+--ALTER TABLE IF EXISTS public.tbl_house_mate
+   -- OWNER to postgres;
 	
 ------Tbl_Maintenance------------------------
 
@@ -323,8 +295,8 @@ CREATE TABLE IF NOT EXISTS public.tbl_maintenance
 (
     issue_id integer NOT NULL DEFAULT nextval('tbl_maintenance_issue_id_seq'::regclass),
     property_id integer NOT NULL DEFAULT nextval('tbl_maintenance_property_id_seq'::regclass),
-    description character varying COLLATE pg_catalog."default",
-    status character varying COLLATE pg_catalog."default",
+    description character varying(255),
+    status character varying(255),
     CONSTRAINT tbl_maintenance_pkey PRIMARY KEY (issue_id),
     CONSTRAINT property_id_foreign_key FOREIGN KEY (property_id)
         REFERENCES public.tbl_property (property_id) MATCH SIMPLE
@@ -332,10 +304,10 @@ CREATE TABLE IF NOT EXISTS public.tbl_maintenance
         ON DELETE NO ACTION
 )
 
-TABLESPACE pg_default;
+--TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.tbl_maintenance
-    OWNER to postgres;
+--ALTER TABLE IF EXISTS public.tbl_maintenance
+    --OWNER to postgres;
 	
 -------Script Rental Agreement-------
 
@@ -348,7 +320,7 @@ CREATE TABLE IF NOT EXISTS public.tbl_rental_agreement
     agreement_id integer NOT NULL DEFAULT nextval('tbl_rental_agreement_agreement_id_seq'::regclass),
     user_id integer NOT NULL DEFAULT nextval('tbl_rental_agreement_user_id_seq'::regclass),
     property_id integer NOT NULL DEFAULT nextval('tbl_rental_agreement_property_id_seq'::regclass),
-    invoices character varying COLLATE pg_catalog."default",
+    invoices character varying(255),
     CONSTRAINT tbl_rental_agreement_pkey PRIMARY KEY (agreement_id),
     CONSTRAINT property_id_foreign_key FOREIGN KEY (property_id)
         REFERENCES public.tbl_property (property_id) MATCH SIMPLE
@@ -360,10 +332,10 @@ CREATE TABLE IF NOT EXISTS public.tbl_rental_agreement
         ON DELETE NO ACTION
 )
 
-TABLESPACE pg_default;
+--TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.tbl_rental_agreement
-    OWNER to postgres;
+--ALTER TABLE IF EXISTS public.tbl_rental_agreement
+    --OWNER to postgres;
 	
 -------Script tbl Support----------------------
 
@@ -375,8 +347,8 @@ CREATE TABLE IF NOT EXISTS public.tbl_support
 (
     support_id integer NOT NULL DEFAULT nextval('tbl_support_support_id_seq'::regclass),
     user_id integer NOT NULL DEFAULT nextval('tbl_support_user_id_seq'::regclass),
-    issue_details character varying(255) COLLATE pg_catalog."default",
-    resolution character varying(255) COLLATE pg_catalog."default",
+    issue_details character varying(255),
+    resolution character varying(255),
     CONSTRAINT tbl_support_pkey PRIMARY KEY (support_id),
     CONSTRAINT user_id_foreign_key FOREIGN KEY (user_id)
         REFERENCES public.tbl_user (user_id) MATCH SIMPLE
@@ -384,10 +356,10 @@ CREATE TABLE IF NOT EXISTS public.tbl_support
         ON DELETE NO ACTION
 )
 
-TABLESPACE pg_default;
+--TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.tbl_support
-    OWNER to postgres;
+--ALTER TABLE IF EXISTS public.tbl_support
+    --OWNER to postgres;
 	
 -------Script Tbl Verification--------
 
@@ -399,7 +371,7 @@ CREATE TABLE IF NOT EXISTS public.tbl_verification
 (
     verification_id integer NOT NULL DEFAULT nextval('tbl_verification_verification_id_seq'::regclass),
     user_id integer NOT NULL DEFAULT nextval('tbl_verification_user_id_seq'::regclass),
-    verified_status character varying(255) COLLATE pg_catalog."default",
+    verified_status character varying(255),
     CONSTRAINT tbl_verification_pkey PRIMARY KEY (verification_id),
     CONSTRAINT user_id_foreign_key FOREIGN KEY (user_id)
         REFERENCES public.tbl_user (user_id) MATCH SIMPLE
@@ -407,10 +379,10 @@ CREATE TABLE IF NOT EXISTS public.tbl_verification
         ON DELETE NO ACTION
 )
 
-TABLESPACE pg_default;
+--TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.tbl_verification
-    OWNER to postgres;
+--ALTER TABLE IF EXISTS public.tbl_verification
+   -- OWNER to postgres;
 	
 
 	
