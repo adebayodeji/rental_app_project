a02b19e810a37f9a87ac69a320f571ebba4311bb
ABASS ADAMO <adamoabasstope@yahoo.com>
2023-12-10 18:55:22 +0100

    create property endpoint on server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 import app from "./fastify.js";
+import { PrismaClient } from "@prisma/client";
 
-app.get("/", async function handler(request, reply) {
-  return { hello: "world" };
-});
-
-try {
-  await app.listen({ port: 3000 });
-} catch (err) {
-  app.log.error(err);
-  process.exit(1);
+export const prisma = new PrismaClient();
+async function main() {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
 }
+
+main()
+  .then(async () => {
+    await prisma.$connect();
+    console.log("Connection to prisma db successful");
+  })
+  .catch(async (e) => {
+    await prisma.$disconnect();
+    process.exit(1);
+  });
