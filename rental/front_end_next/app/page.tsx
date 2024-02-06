c74a91c96aae039392c0d495039f2fb1c7c45059
ABASS ADAMO <adamoabasstope@yahoo.com>
2024-02-06 20:40:18 +0100

    swap asset icons for icons-material

--- rental/front_end_next/app/page.tsx
+++ rental/front_end_next/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import SignUpForm from './components/forms/SignUpForm'
-import SignupPage from './users/signup/page'
-import HomePage from './users/homepage/page'
+import Image from "next/image";
+import SignUpForm from "./components/forms/SignUpForm";
+import SignupPage from "./users/signup/page";
+import HomePage from "./users/homepage/page";
+import AppFooter from "./components/ui/AppFooter";
 
 export default function Home() {
   return (
     <main>
-      <HomePage />
+      <AppFooter />
     </main>
-  )
+  );
 }
